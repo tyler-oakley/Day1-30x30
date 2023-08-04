e1df65d89c6b3bd69e56968e84d5f0d6ed66888f
Tyler Oakley <tyler.oakley@briebug.com>
2023-08-04 08:56:22 -0600

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'shopping-list';
+  title = 'Shopping List';
+
+  // grocery list holding mock data
+  groceryList: GroceryItem[] = [
+    { name: 'Bananas' },
+    { name: 'Apples' },
+    { name: 'Oranges' },
+    { name: 'Bread' },
+    { name: 'Milk' },
+    { name: 'Chicken' }
+  ];
+
+  onAddItem(name: string) {
+    this.groceryList.push(new GroceryItem(name));
+  }
+}
+
+export class GroceryItem {
+  public name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
 }
